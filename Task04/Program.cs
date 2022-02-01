f9f56fe98453cc13d75706647d5a2c10ee38e21e
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-02-01 19:02:06 +0300

    Task04 done

---
+++ Task04/Program.cs
@@ -0,0 +1,31 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()??"0");
+Console.Write("Задайте размер массива: ");
+int arrayLength = int.Parse(Console.ReadLine()??"0");
+int [] array = new int [arrayLength];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,11);
+}
+Console.Write("Массив: " );
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+bool ContainsNumber(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+      if (array[i] == number)
+         return true;
+   return false;      
+}
+Console.WriteLine();
+if (ContainsNumber(array) == true)
+{
+   Console.Write($"В массиве есть число соответствующее {number} ");
+}
+else
+{
+   Console.Write($"В массиве нет числа соответсвующего {number} ");
+}
